7093105234118f1d4a25b549634e226164120962
Giovana <giovanacarvalho@brq.com>
2021-08-03 18:05:17 -0300

    refactor: create a file for exports validations

---
+++ src/modules/passports/validators/index.ts
@@ -0,0 +1,5 @@
+export * from './birth-date-validator';
+export * from './cpf-validator';
+export * from './nationality-validator';
+export * from './visa-validator';
+export * from './passport-validator';
